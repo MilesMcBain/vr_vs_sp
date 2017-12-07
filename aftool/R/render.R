3f42d78681313d006f28365d56f0ccbd1fedb875
Miles McBain <miles.mcbain@gmail.com>
2017-12-07 17:41:59 +1000

    A Fiery implementation... JSON stuff not working yet

--- aftool/R/render.R
+++ aftool/R/render.R
@@ -6,27 +6,38 @@ af_render <- function(scene_title, object){
   template_args$file <- object
   af_html <- purrr::map(template, ~stringr::str_interp(., template_args))
 
-  vr_app <- list(
-    call = function(req){
-      list(
-        status = 200L,
-        headers = list(
-          'Content-Type' = 'text/html'
-        ),
-        body = paste0(af_html, collapse = "\r\n")
-      )
-   }
-  )
+  # Create request router for VR app
+  request_router <- routr::Route$new()
 
-  httpuv::runServer(host = "0.0.0.0",
-                    port = 8080,
-                    app = vr_app)
+  # Handler for app root
+  handle_root <- function(request, response, keys, ...) {
+    response$status <- 200L
+    response$type <- 'html'
+    response$body <- paste0(af_html, collapse = "\r\n")
+    return(FALSE)
+  }
 
-  # Create handler router for VR app
+  # Handler for object JSON file
+  handle_object <- function(request, response, keys, ...){
+    response$status <- 200L
+    response$type <- "json"
+    response$body <- readr::read_lines(object)
+  }
 
+  # Attach handlers
+  request_router$add_handler('get', "/", handle_root)
+  request_router$add_handler('get', gsub(pattern = "^\\.",replacement = "", object), handle_object)
+
+  # Create Route Stack
+  routr_stack <- routr::RouteStack$new()
+  routr_stack$add_route(request_router)
 
   # Create VR app
-
+  app <- fiery::Fire$new()
+  app$attach(routr_stack)
+  app$ignite(block = TRUE)
+  # In Terminal (or visit in browser)
+  # curl http://127.0.0.1:8080/
 }
 
 
