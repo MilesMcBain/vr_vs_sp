fe80bf88e0e050f59a62e5c270b367ba1c079299
Miles McBain <miles.mcbain@gmail.com>
2017-12-14 17:06:56 +1000

    ACP mesh working, yet to view in VR, scaling will be off.

--- alexander_clark_park/QSC_Extracted_Data_20171212_140035727000-20064/acp.R
+++ alexander_clark_park/QSC_Extracted_Data_20171212_140035727000-20064/acp.R
@@ -2,21 +2,47 @@ library(sf)
 library(raster)
 library(anglr) ## devtools::install_github("hypertidy/anglr")
 
-nc <- read_sf("./Contours_1_metre.shp")
-p_mesh <- anglr(nc, max_area = 0.008)
+# read shape file:
+acp <- read_sf("./alexander_clark_park/QSC_Extracted_Data_20171212_140035727000-20064/Contours_1_metre.shp")
 
-vertexes <- reduce(list(p_mesh$v, p_mesh$lXv, p_mesh$l, p_mesh$o), left_join)
-vertex_dupes <- which(duplicated(vertexes$vertex_))
-p_mesh$v$z_ <- vertexes$ELEVATION[-vertex_dupes]
+# convert contours to raster
+acp_elev_raster <- 
+  contour_2_raster(acp, 
+                   n_samples = 100000,
+                   raster_attribute = "ELEVATION",
+                   ncol = 120,
+                   nrow = 120)
+
+# Create an extent to triangulate
+acp_extent <- st_as_sf(as(extent( st_bbox(acp)[c(1, 3, 2, 4)] + c(1, -1, 1, -1) * 0.0001 ), "SpatialPolygons"))
+acp_extent$z <- 0
+# Set its spatial meta data
+st_crs(acp_extent) <- st_crs(acp)
+
+# make a mesh and set z to our raster
+acp_extent_mesh <- anglr(acp_extent, max_area = 0.00000001)
+
+acp_extent_mesh$v$z_ <- 
+  raster::extract(acp_elev_raster/12000, # This is approximately right, I think.
+                  cbind(acp_extent_mesh$v$x_, acp_extent_mesh$v$y_), method = "bilinear")
+acp_extent_mesh$v <- tidyr::fill(acp_extent_mesh$v, z_)
+acp_extent_mesh$v$z_
+
+rgl.clear()
+plot(acp_extent_mesh)
+
+# Determine the face colours
+
+
 
 library(tidyverse)
-vertices <- 
-   p_mesh$v %>%
+vertices <-
+   acp_extent_mesh$v %>%
    mutate(v_ind = seq(0, n()-1)) %>%
    mutate_if(is_double, ~scale(., center = TRUE, scale = FALSE))
 
-faces_3js <- 
-   p_mesh$tXv %>%
+faces_3js <-
+   acp_extent_mesh$tXv %>%
    left_join(vertices) %>%
    select(triangle_, v_ind) %>%
    nest(v_ind) %>%
@@ -25,8 +51,8 @@ faces_3js <-
    unlist() %>%
    paste0(" 2, ", .,", 0", collapse=",")
 
-vertices_3js <- 
-  vertices %>% 
+vertices_3js <-
+  vertices %>%
   select(x_, y_, z_) %>%
   transpose() %>%
   map( ~paste0(., collapse=",")) %>%
@@ -39,7 +65,7 @@ uvs_3js <- ""
 ## format out to JSON (I'm not at all sure about the uvs but it might not matter)
 output <- sprintf(
   '{
-  "metadata": { "formatVersion" : 3 },	
+  "metadata": { "formatVersion" : 3 },
 
   "materials": [ {"DbgColor": 15597568,
                 "DbgIndex": 1,
@@ -60,10 +86,10 @@ output <- sprintf(
   "normals":  [ %s ],
   "colors":   [ %s ],
   "uvs":      [ %s ],
-  "faces": [ 
+  "faces": [
   %s
   ]}
-  ', 
+  ',
   vertices_3js, normals_3js, colors_3js, uvs_3js, faces_3js)
   writeLines(output, "test_acp.json")
 
